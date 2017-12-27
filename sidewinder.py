09128e1141d375077d3e445f822505a7a7d521f7
Solomon Bothwell <ssbothwell@gmail.com>
2017-12-26 23:44:48 -0800

    (REFACTOR) Refactors sidewinder

--- sidewinder.py
+++ sidewinder.py
@@ -5,26 +5,30 @@ Solomon Bothwell
 
 Based on examples from "Mazes For Programmers" by Jamis Buck
 """
+from typing import Any
 from random import randint
 from grid import Grid
 
 
-def sidewinder(grid: Grid) -> Grid:
-    for row in reversed(grid.grid):
+def pick_random(arr: list) -> Any:
+    """ Return a random element from an array """
+    return arr[randint(0, len(arr) - 1)]
+
+
+def sidewinder(grid: Grid) -> None:
+    for cell in reversed(grid):
         run = []
-        for cell in row:
-            run.append(cell)
-            choice = randint(0, 1)
-            if ((choice == 1 and cell.north is not None) or 
-                cell.east is None):
-                close_out = True
-                index = randint(0, len(run) - 1)
-                run_choice = run[index]
-                if run_choice.north:
-                    run_choice.link(run_choice.north)
-                run = []
-            else:
-                cell.link(cell.east)
+        run.append(cell)
+        choice = randint(0, 1)
+        if ((choice == 1 and cell.north is not None) or 
+            cell.east is None):
+            close_out = True
+            run_choice = pick_random(run) 
+            if run_choice.north:
+                run_choice.link(run_choice.north)
+            run = []
+        else:
+            cell.link(cell.east)
 
 if __name__ == "__main__":
     g = Grid(4,4)
