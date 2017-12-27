ecf9e2f57aaaa7b36b88be6d6f90077df2047415
Solomon Bothwell <ssbothwell@gmail.com>
2017-12-26 23:41:16 -0800

    (REFACTOR) adds pick_random func to binary_tree

--- binary_tree.py
+++ binary_tree.py
@@ -5,16 +5,21 @@ Solomon Bothwell
 
 Based on examples from "Mazes For Programmers" by Jamis Buck
 """
+from typing import Any
 from random import randint
 from grid import Grid
+
+
+def pick_random(arr: list) -> Any:
+    """ Return a random element from an array """
+    return arr[randint(0, len(arr) - 1)]
 
 
 def binary_tree(grid: Grid) -> None:
     for cell in reversed(grid):
         neighbors = list(filter(lambda x: x != None, [cell.north, cell.east]))
         if neighbors:
-            index = randint(0, len(neighbors) -1)
-            neighbor = neighbors[index]
+            neighbor = pick_random(neighbors)
             cell.link(neighbor)
 
 
